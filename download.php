11875ef61fb0cf428c1712ad3a9aaca07a72bb90
Jan Kruse <jan@jans-mbp.speedport_w_723v_1_40_000>
2016-04-16 20:59:14 +0200

    Remove Google Analytics script

--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 <?php
 
 	require_once "lib/Dropbox/autoload.php";
-    require_once "config/config.php";
+	require_once "config/config.php";
     
 	use \Dropbox as dbx;
 	
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 	require_once "lib/Dropbox/autoload.php";
-    require_once "config/config.php";
+	require_once "config/config.php";
     
 	use \Dropbox as dbx;
 	
@@ -21,17 +21,6 @@
 			<link rel="stylesheet" href="css/bootstrap.min.css">
             
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		
-            <script>
-                (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
-
-                ga("create", "UA-50355891-2", "auto");
-                ga("send", "pageview");
-
-            </script>
         </head>
 		<body>
 			<br/>
